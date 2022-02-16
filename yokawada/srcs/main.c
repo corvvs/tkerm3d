06cfc04b1005262b4a9430d42c2e71348566b252
河田鷹介 <benefium@gmail.com>
2022-02-16 21:11:38 +0900

    回転軸回転

--- yokawada/srcs/main.c
+++ yokawada/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: corvvs <corvvs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 22:57:24 by corvvs            #+#    #+#             */
-/*   Updated: 2022/02/16 11:23:16 by corvvs           ###   ########.fr       */
+/*   Updated: 2022/02/16 18:57:51 by corvvs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,12 +51,13 @@ void	t3_init_optics(t_system *system)
 	system->optics.offset_y = 4;
 	system->optics.sq_size_x = (double)(2 * system->optics.offset_x) / T3_WIDTH;
 	system->optics.sq_size_y = (double)(2 * system->optics.offset_y) / T3_HEIGHT;
+	memcpy(system->optics.rot_axis, (t_vector3d){0, 1, 0}, sizeof(double) * 3);
 	system->optics.phi = 0;
 	// 2秒で半周するように角速度を設定
 	system->optics.omega = M_PI / 1000000;
 	// 1000000マイクロ秒(=1秒)をFPSで割ると、1フレームあたりのマイクロ秒数(uspf)が算出される。
 	// これを使ってFPSをコントロールする(もっといい方法がありそう)。
-	system->optics.fps = 30;
+	system->optics.fps = 120;
 	system->optics.uspf = 1000000 / system->optics.fps;
 }
 
@@ -111,47 +112,26 @@ int t3_get_key(void)
 
 void	t3_update_by_key(t_system *system, int key)
 {
-	if (key == 'e')
+	if (key == 'e' || key == 'q')
 	{
-		system->optics.omega += 0.000001;
+		system->optics.omega += ((key == 'e') * 2 - 1) * 0.000001;
 		dprintf(STDERR_FILENO, "omega -> %f[rot/s]\n", system->optics.omega * 1000000 / M_PI / 2);
 	}
-	else if (key == 'q')
+	else if (key == 'd' || key == 'a')
 	{
-		system->optics.omega -= 0.000001;
-		dprintf(STDERR_FILENO, "omega -> %f[rot/s]\n", system->optics.omega * 1000000 / M_PI / 2);
-	}
-	else if (key == 'a')
-	{
-		system->optics.offset_x -= 0.1;
+		system->optics.offset_x += ((key == 'd') * 2 - 1) * 0.1;
 		dprintf(STDERR_FILENO, "offset.x -> %f\n", system->optics.offset_x);
 	}
-	else if (key == 'd')
+	else if (key == 's' || key == 'w')
 	{
-		system->optics.offset_x += 0.1;
-		dprintf(STDERR_FILENO, "offset.x -> %f\n", system->optics.offset_x);
+		system->optics.offset_y += ((key == 'w') * 2 - 1) * 0.1;
+		dprintf(STDERR_FILENO, "offset.y -> %f\n", system->optics.offset_x);
 	}
-	else if (key == 'w')
+	else if (key == 'x' || key == 'z')
 	{
-		system->optics.offset_y -= 0.1;
-		dprintf(STDERR_FILENO, "offset.y -> %f\n", system->optics.offset_y);
-	}
-	else if (key == 's')
-	{
-		system->optics.offset_y += 0.1;
-		dprintf(STDERR_FILENO, "offset.y -> %f\n", system->optics.offset_y);
-	}
-	else if (key == 'x')
-	{
-		if (system->optics.fps < 240)
-			system->optics.fps += 1;
-		system->optics.uspf = 1000000 / system->optics.fps;
-		dprintf(STDERR_FILENO, "fps -> %f\n", system->optics.fps);
-	}
-	else if (key == 'z')
-	{
-		if (2 < system->optics.fps)
-			system->optics.fps -= 1;
+		if ((key == 'x' && system->optics.fps < 240) ||
+			(key == 'z' && 2 < system->optics.fps))
+			system->optics.fps += ((key == 'x') * 2 - 1);
 		system->optics.uspf = 1000000 / system->optics.fps;
 		dprintf(STDERR_FILENO, "fps -> %f\n", system->optics.fps);
 	}
@@ -165,6 +145,27 @@ void	t3_update_by_key(t_system *system, int key)
 		system->optics.scale_factor *= 1.05;
 		dprintf(STDERR_FILENO, "scale -> %f\n", system->optics.scale_factor);
 	}
+	else if (key == '3')
+	{
+		t3_rot_vector_around_axis(system->optics.rot_axis,
+			(t_vector3d){1, 0, 0}, M_PI / 100);
+		dprintf(STDERR_FILENO, "rot axis -> (%f, %f, %f)\n",
+			system->optics.rot_axis[0], system->optics.rot_axis[1], system->optics.rot_axis[2]);
+	}
+	else if (key == '4')
+	{
+		t3_rot_vector_around_axis(system->optics.rot_axis,
+			(t_vector3d){0, 1, 0}, M_PI / 100);
+		dprintf(STDERR_FILENO, "rot axis -> (%f, %f, %f)\n",
+			system->optics.rot_axis[0], system->optics.rot_axis[1], system->optics.rot_axis[2]);
+	}
+	else if (key == '5')
+	{
+		t3_rot_vector_around_axis(system->optics.rot_axis,
+			(t_vector3d){0, 0, 1}, M_PI / 100);
+		dprintf(STDERR_FILENO, "rot axis -> (%f, %f, %f)\n",
+			system->optics.rot_axis[0], system->optics.rot_axis[1], system->optics.rot_axis[2]);
+	}
 	else if (key == ' ')
 	{
 		system->optics.animate = !system->optics.animate;
@@ -175,25 +176,29 @@ void	t3_update_by_key(t_system *system, int key)
 // [描画ループ]
 void	t3_render_loop(t_system *system)
 {
-	t_ut	t0 = t3_get_ut();
-	t_ut	t1;
-	int		key_pressed;
+	t_ut		t0 = t3_get_ut();
+	t_ut		t1;
+	int			key_pressed;
+	t_optics	*opt;
 
+	opt = &system->optics;
 	while (true)
 	{
 		t3_affine_scale(system->transform_animated, system->transform_static,
-			(t_vector3d){system->optics.scale_factor, system->optics.scale_factor, system->optics.scale_factor});
-		t3_affine_rot_y(system->transform_animated, system->transform_animated, system->optics.phi);
+			(t_vector3d){opt->scale_factor, opt->scale_factor, opt->scale_factor});
+		t3_affine_rot_axis(system->transform_animated, system->transform_animated, opt->rot_axis, opt->phi);
+		t3_affine_scale(system->transform_animated, system->transform_animated,
+			(t_vector3d){1, -1, 1});
 		t3_affine_translate(system->transform_animated, system->transform_animated,
-			(t_vector3d){system->optics.offset_x, system->optics.offset_y, 0});
+			(t_vector3d){opt->offset_x, opt->offset_y, 0});
 		t3_transform_to_render(system);
 		t3_clear_pixelbuffer(system);
 		t3_fill_pixelbuffer(system);
-		t1 = t3_wait_until(t0 + system->optics.uspf);
+		t1 = t3_wait_until(t0 + opt->uspf);
 		t3_render_pixel_buffer(system);
 		// powとかsinがある係数は、回転速度をいい感じに揺らすためにかけている
-		if (system->optics.animate)
-			system->optics.phi += 2 * (0.1 + pow(sin(system->optics.phi), 4)) * system->optics.omega * (t1 - t0);
+		if (opt->animate)
+			opt->phi += 2 * (0.1 + pow(sin(opt->phi), 0)) * opt->omega * (t1 - t0);
 		t0 = t1;
 		key_pressed = t3_get_key();
 		if (key_pressed)
